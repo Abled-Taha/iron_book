1a01b51e7a6def6be7ce78ca78d1a75936473d0e
Abled-Taha <abledtaha@gmail.com>
2026-06-06 21:46:16 +0500

    Remove local db in settings.py

--- apps/web/iron_book/settings.py
+++ apps/web/iron_book/settings.py
@@ -68,13 +68,6 @@ AUTHENTICATION_BACKENDS = [
 
 WSGI_APPLICATION = "iron_book.wsgi.application"
 
-# DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-# }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",

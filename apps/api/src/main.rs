4f5f87fa0a9251308ebc089815caa010b7416235
Abled-Taha <abledtaha@gmail.com>
2026-07-07 22:58:15 +0500

    Add placeholder endpoints and feature class for auth

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -10,7 +10,9 @@ pub async fn main() {
         .route("/", get(views::system::root))
         .route("/health", get(views::system::health))
         .route("/users/{id}", get(views::users::get_by_id))
-        .route("/users/search", get(views::users::search));
+        .route("/users/search", get(views::users::search))
+        .route("/auth/register", get(views::auth::register))
+        .route("/auth/login", get(views::auth::login));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
---
+++ apps/api/src/views/auth.rs
@@ -0,0 +1,9 @@
+pub async fn register() -> String {
+    //Should have some actual logic
+    format!("Registering") // This is a text response
+}
+
+pub async fn login() -> String {
+    //Should have some actual logic
+    format!("Logging in") // This is a text response
+}

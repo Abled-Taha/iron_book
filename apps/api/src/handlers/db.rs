862bbf3eb6c00947b7a49fbe91bc7eb63a27ace9
Abled-Taha <abledtaha@gmail.com>
2026-07-11 00:27:47 +0500

    DB uses credentials from .env

--- apps/api/src/handlers/db.rs
+++ apps/api/src/handlers/db.rs
@@ -1,8 +1,12 @@
+use crate::handlers::config;
 use crate::structs::Db;
 use sqlx::postgres::PgPoolOptions;
 
 pub async fn connect() -> Db {
-    let db_url = "postgres://user:password@localhost:5432/ironbook";
+    let db_user = config::get("POSTGRES_USER");
+    let db_password = config::get("POSTGRES_PASSWORD");
+    let db_name = config::get("POSTGRES_DATABASE");
+    let db_url = &format!("postgres://{db_user}:{db_password}@localhost:5432/{db_name}");
 
     let pool = PgPoolOptions::new()
         .max_connections(5)

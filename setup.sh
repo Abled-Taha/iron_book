9c1427f45c41e57f5d35b1fe0537d9a6fd49abe5
Abled-Taha <abledtaha@gmail.com>
2026-08-20 19:55:46 +0500

    Update setup.sh to comment some env checking code

--- setup.sh
+++ setup.sh
@@ -18,20 +18,20 @@ get_linux_distro() {
 DISTRO=$(get_linux_distro)
 
 # 1. Safely handle the .env creation so it doesn't overwrite an existing file
-if [ ! -f ./apps/web/.env ]; then
-  cp ./apps/web/.env.example ./apps/web/.env
-fi
-if [ ! -f ./apps/api/.env ]; then
-  cp ./apps/api/.env.example ./apps/api/.env
-fi
-if [ ! -f ./.env ]; then
-  cp ./.env.example ./.env
-fi
+# if [ ! -f ./apps/web/.env ]; then
+#   cp ./apps/web/.env.example ./apps/web/.env
+# fi
+# if [ ! -f ./apps/api/.env ]; then
+#   cp ./apps/api/.env.example ./apps/api/.env
+# fi
+# if [ ! -f ./.env ]; then
+#   cp ./.env.example ./.env
+# fi
 
-read -n 1 -r -s -p "Kindly verify the entries in ./apps/web/.env, then press any key to continue." key
-echo ""
-read -n 1 -r -s -p "Kindly verify the entries in ./apps/api/.env, then press any key to continue." key
-echo ""
+# read -n 1 -r -s -p "Kindly verify the entries in ./apps/web/.env, then press any key to continue." key
+# echo ""
+# read -n 1 -r -s -p "Kindly verify the entries in ./apps/api/.env, then press any key to continue." key
+# echo ""
 
 case "$DISTRO" in
 nixos)

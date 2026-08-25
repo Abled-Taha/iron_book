8105e04378b370dffe1a50055c79dc860fd59d39
Abled-Taha <abledtaha@gmail.com>
2026-08-26 01:19:16 +0500

    Cleanup

--- setup.sh
+++ setup.sh
@@ -18,20 +18,24 @@ get_linux_distro() {
 DISTRO=$(get_linux_distro)
 
 # 1. Safely handle the .env creation so it doesn't overwrite an existing file
-# if [ ! -f ./apps/web/.env ]; then
-#   cp ./apps/web/.env.example ./apps/web/.env
-# fi
-# if [ ! -f ./apps/api/.env ]; then
-#   cp ./apps/api/.env.example ./apps/api/.env
-# fi
-# if [ ! -f ./.env ]; then
-#   cp ./.env.example ./.env
-# fi
-
-# read -n 1 -r -s -p "Kindly verify the entries in ./apps/web/.env, then press any key to continue." key
-# echo ""
-# read -n 1 -r -s -p "Kindly verify the entries in ./apps/api/.env, then press any key to continue." key
-# echo ""
+if [ ! -f ./apps/android/.env ]; then
+  cp ./apps/android/.env.example ./apps/android/.env
+fi
+if [ ! -f ./apps/api/.env ]; then
+  cp ./apps/api/.env.example ./apps/api/.env
+fi
+if [ ! -f ./apps/desktop/.env ]; then
+  cp ./apps/desktop/.env.example ./apps/desktop/.env
+fi
+if [ ! -f ./apps/home/.env ]; then
+  cp ./apps/home/.env.example ./apps/home/.env
+fi
+if [ ! -f ./apps/web/.env ]; then
+  cp ./apps/web/.env.example ./apps/web/.env
+fi
+if [ ! -f ./.env ]; then
+  cp ./.env.example ./.env
+fi
 
 case "$DISTRO" in
 nixos)
